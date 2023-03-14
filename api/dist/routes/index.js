8d833f0605e8dda79250e1b57ccd30c10814ec24
Moo <sukorn@gmail.com>
2023-03-14 23:05:29 +0700

    discard build files

--- api/dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-const express_1 = require("express");
-const defaultRoute_1 = require("./defaultRoute");
-const auth_1 = require("./auth");
-exports.routes = (0, express_1.Router)();
-exports.routes.use(defaultRoute_1.defaultRoute);
-exports.routes.use(auth_1.authRouter);
